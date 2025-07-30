ed27957f27c779ece8216e9476f27bc6c07f8b7c
kaladin <335095@niuitmo.ru>
2025-07-30 13:16:14 +0300

    add tolk ton vault and liq deploy contracts

--- sources/tolk-toolchain/sources.ts
+++ sources/tolk-toolchain/sources.ts
@@ -1,7 +1,8 @@
 // source map for contracts paths,
 // entrypoint is 'sources/contracts/...'
 export const DEX_SOURCES = {
-    "ton-vault": "sources/contracts/vaults/ton-vault.tolk",
+    "ton-vault": "sources/contracts/tolk/ton-vault.tolk",
+    "liquidity-deposit": "sources/contracts/tolk/liquidity-deposit.tolk",
 }
 
 export type ContractName = keyof typeof DEX_SOURCES
