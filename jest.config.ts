dab1a18ca7329fd1c4f018281b802cbea11d931b
kaladin <335095@niuitmo.ru>
2025-08-12 11:13:18 +0300

    increase jest timeout

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,5 @@ export default {
     preset: "ts-jest",
     testEnvironment: "node",
     testPathIgnorePatterns: ["/node_modules/", "/dist/", "proofs.spec.ts", "factory.spec.ts"],
+    testTimeout: 30000, // 30 seconds timeout for all tests
 }
